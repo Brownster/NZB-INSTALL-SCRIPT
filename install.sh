f19e4307cc3e25404d8af5d560eb75a807d3a438
Brownster <marcsmobile.brown@googlemail.com>
2014-01-31 13:29:41 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -317,6 +317,8 @@ sleep 2
 mkdir /home/$username/.pid/
 mkdir /home/$username/temp
 mkdir /home/$username/.couchpotato
+mkdir /home/$username/.headphones
+mkdir /home/$username/.lazylibrarian
 mkdir /home/downloads
 mkdir /home/downloads/completed
 mkdir /home/downloads/completed/tv
