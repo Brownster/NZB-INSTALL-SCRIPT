92e8ce9ddf488330b78d1729d64f32085c123345
Brownster <marcsmobile.brown@googlemail.com>
2014-02-02 16:35:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -214,32 +214,40 @@ echo "##############################"
 sleep 3
 
 cat > /etc/sysctl.conf << EOF
+# IP Spoofing protection
 net.ipv4.conf.all.rp_filter = 1
 net.ipv4.conf.default.rp_filter = 1
+
 # Ignore ICMP broadcast requests
 net.ipv4.icmp_echo_ignore_broadcasts = 1
+
 # Disable source packet routing
 net.ipv4.conf.all.accept_source_route = 0
-net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.all.accept_source_route = 0 
 net.ipv4.conf.default.accept_source_route = 0
 net.ipv6.conf.default.accept_source_route = 0
-# Ignore send rediretcs
-net.ipv4.conf.all.send_rediretcs = 0
-net.ipv4.conf.default.send_rediretcs = 0
+
+# Ignore send redirects
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+
 # Block SYN attacks
 net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_max_syn_backlog = 2048
 net.ipv4.tcp_synack_retries = 2
 net.ipv4.tcp_syn_retries = 5
+
 # Log Martians
 net.ipv4.conf.all.log_martians = 1
 net.ipv4.icmp_ignore_bogus_error_responses = 1
-# Ignore ICMP rediretcs
-net.ipv4.conf.all.accept_rediretcs = 0
-net.ipv6.conf.all.accept_rediretcs = 0
-net.ipv4.conf.default.accept_rediretcs = 0
-net.ipv6.conf.default.accept_rediretcs = 0
-# Ignore Diretced pings
+
+# Ignore ICMP redirects
+net.ipv4.conf.all.accept_redirects = 0
+net.ipv6.conf.all.accept_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0 
+net.ipv6.conf.default.accept_redirects = 0
+
+# Ignore Directed pings
 net.ipv4.icmp_echo_ignore_all = 1
 EOF
 sysctl -p
@@ -696,8 +704,7 @@ chmod 777 /home/$username/.lazylibrarian
 chmod +x /etc/init.d/lazylibrarian  
 update-rc.d lazylibrarian  defaults  
 echo "starting Lazy Librarian on port $BOOKPORT"
-python /home/$username/.lazylibrarian/LazyLibrarian.py --daemon
-echo "Lazy Librarian has started you can try http://$HOSTIP:$BOOKPORT"
+echo "Lazy Librarian will start on nect boot you can access the ui via http://$HOSTIP:$BOOKPORT"
 sleep 2
 
 echo "########################"
