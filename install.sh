1c11cc22a5911e5e8e29e0dd6a5604b3e78a787e
Brownster <marcsmobile.brown@googlemail.com>
2014-02-03 12:23:32 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -384,62 +384,7 @@ echo "backing up sickbeard"
 cp sickbeard /home/backups/sickbeard
 mv sickbeard /home/$username/.sickbeard
 #cp /home/$username/.sickbeard/config.ini /etc/default/sickbeard
-#cp /home/$username/.sickbeard/init.ubuntu /etc/init.d/sickbeard
-
-cat > /etc/init.d/sickbeard << EOF
-#! /bin/sh
-# Author: daemox
-# Basis: Parts of the script based on and inspired by work from
-# tret (sabnzbd.org), beckstown (xbmc.org),
-# and midgetspy (sickbeard.com).
-# Fixes: Alek (ainer.org), James (ainer.org), Tophicles (ainer.org),
-# croontje (sickbeard.com)
-# Contact: http://www.ainer.org
-# Version: 3.1
-### BEGIN INIT INFO
-# Provides: sickbeard
-# Required-Start: $local_fs $network $remote_fs
-# Required-Stop: $local_fs $network $remote_fs
-# Should-Start: $NetworkManager
-# Should-Stop: $NetworkManager
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: starts and stops sick beard
-# Description: Sick Beard is an Usenet PVR. For more information see:
-# http://www.sickbeard.com
-### END INIT INFO
-#Required -- Must Be Changed!
-USER="$username" #Set Linux Mint, Ubuntu, or Debian user name here.
-
-#Required -- Defaults Provided (only change if you know you need to).
-HOST="$HOSTIP" #Set Sick Beard address here.
-PORT="$SICKPORT" #Set Sick Beard port here.
-
-#Optional -- Unneeded unless you have added a user name and password to Sick Beard.
-SBUSR="$WEBUSER" #Set Sick Beard user name (if you use one) here.
-SBPWD="$WEBPASS" #Set Sick Beard password (if you use one) here.
-
-#Script -- No changes needed below.
-case "$1" in
-start)
-#Start Sick Beard and send all messages to /dev/null.
-cd /home/$username/.sickbeard
-echo "Starting Sick Beard"
-sudo -u $USER -EH nohup python /home/$USER/.sickbeard/SickBeard.py -q &gt; /dev/null 2&gt;&amp;1 &amp;
-;;
-stop)
-#Shutdown Sick Beard and delete the index.html files that wget generates.
-echo "Stopping Sick Beard"
-wget -q --user=$SBUSR --password=$SBPWD "http://$HOST:$PORT/home/shutdown/" --delete-after
-sleep 6s
-;;
-*)
-echo "Usage: $0 {start|stop}"
-exit 1
-esac
-exit 0
-EOF
-
+cp /home/$username/.sickbeard/init.ubuntu /etc/init.d/sickbeard
 
 cat > /etc/default/sickbeard << EOF
 [General]
@@ -888,7 +833,7 @@ cp /home/$username/temp/lazylibrarian /home/backups/lazylibrarian/
 mv /home/$username/temp/lazylibrarian  /home/$username/.lazylibrarian 
 cp /home/$username/.lazylibrarian/init/ubuntu.initd /etc/init.d/lazylibrarian
 
-cat > /etc/default/lazylibrarian <<EOF
+cat > /etc/default/lazylibrarian << EOF
 APP_PATH=/home/castro/.lazylibrarian
 ENABLE_DAEMON=1
 RUN_AS=$user
