632f0a5ddf356cf967f660a1c83e1b14fde3ba81
Brownster <marcsmobile.brown@googlemail.com>
2014-11-04 15:42:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -150,216 +150,215 @@ do
  read opt
  case $opt in
 
-8)
-echo "#######################"
-echo "## create a new user ##"
-echo "#######################"
+8) 	echo "#######################"
+	echo "## create a new user ##"
+	echo "#######################"
 
-echo "we will add a user so we can stop using root, please provide username and password when prompted"
-sleep2
-if [ $(id -u) -eq 0 ]; then
-	read -p "Enter username : " username
-	read -s -p "Enter password : " password
-	egrep "^$username" /etc/passwd >/dev/null
-	if [ $? -eq 0 ]; then
-		echo "$username exists!"
-		exit 1
+	echo "we will add a user so we can stop using root, please provide username and password when prompted"
+	sleep2
+	if [ $(id -u) -eq 0 ]; then
+		read -p "Enter username : " username
+		read -s -p "Enter password : " password
+		egrep "^$username" /etc/passwd >/dev/null
+		if [ $? -eq 0 ]; then
+			echo "$username exists!"
+			exit 1
+		else
+			pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+			useradd -m -p $pass $username
+			[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+		fi
 	else
-		pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-		useradd -m -p $pass $username
-		[ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
-	fi
-else
-	echo "Only root may add a user to the system"
-	exit 2
-	fi
+		echo "Only root may add a user to the system"
+		exit 2
+		fi
 
 #just in case we dont have git
-apt-get install git -y
+	apt-get install git -y
 #install python now incase sabnzbsplus install fails
-apt-get install python-cheetah -y
+	apt-get install python-cheetah -y
 
-echo "####################"
-echo "## installing ufw ##"
-echo "####################"
-sleep 2
-apt-get install ufw -y
-
-
-echo "###############################"
-echo "## opening ports on firewall ##"
-echo "###############################"
-ufw allow $SSHPORT
-echo "opening old ssh port just for now to make sure we dont lose our connetcion"
-ufw allow ssh
-echo "opening new Sab web UI port"
-ufw allow $SABPORT
-echo "opening new Sickbeard web UI port"
-ufw allow $SICKPORT
-echo "opening new Couchpotato web UI port"
-ufw allow $COUCHPORT
-echo "opening new Headphones web UI port"
-ufw allow $HEADPORT
-echo "opening new Lazy Librarian web UI port"
-ufw allow $BOOKPORT
-echo "opening new Squid Proxy server Port"
-ufw allow $SQUIDPORT
-echo "opening new Transmission web UI Port"
-ufw allow $TRANPORT
-echo "opening port for Maraschino"
-ufw allow $MARAPORT
-echo "nZEDb web port"
-ufw allow 80
-echo "editing sshd config"
-sed -i "s/port 22/port $sshport/" /etc/ssh/sshd_config
-sed -i "s/protocol 3,2/protocol 2/" /etc/ssh/sshd_config
-sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
-sed -i "s/DebianBanner yes/DebianBanner no/" /etc/ssh/sshd_config
-echo "restarting ssh"
-sleep 2
-/etc/init.d/ssh restart -y
-echo "enabling firewall"
-sleep 2
-ufw enable -y
+	echo "####################"
+	echo "## installing ufw ##"
+	echo "####################"
+	sleep 2
+	apt-get install ufw -y
 
 
-echo "##########################"
-echo "## secure shared memory ##"
-echo "##########################"
-sleep 2
-echo "tmpfs     /run/shm     tmpfs     defaults,noexec,nosuid     0     0" >> /etc/fstab
-echo "adding admin group"
-sleep 2
-groupadd admin
-usermod -a -G admin $username
-echo "protect su by limiting access to admin group only"
-dpkg-statoverride --update --add $username admin 4750 /bin/su
+	echo "###############################"
+	echo "## opening ports on firewall ##"
+	echo "###############################"
+	ufw allow $SSHPORT
+	echo "opening old ssh port just for now to make sure we dont lose our connetcion"
+	ufw allow ssh
+	echo "opening new Sab web UI port"
+	ufw allow $SABPORT
+	echo "opening new Sickbeard web UI port"
+	ufw allow $SICKPORT
+	echo "opening new Couchpotato web UI port"
+	ufw allow $COUCHPORT
+	echo "opening new Headphones web UI port"
+	ufw allow $HEADPORT
+	echo "opening new Lazy Librarian web UI port"
+	ufw allow $BOOKPORT
+	echo "opening new Squid Proxy server Port"
+	ufw allow $SQUIDPORT
+	echo "opening new Transmission web UI Port"
+	ufw allow $TRANPORT
+	echo "opening port for Maraschino"
+	ufw allow $MARAPORT
+	echo "nZEDb web port"
+	ufw allow 80
+	echo "editing sshd config"
+	sed -i "s/port 22/port $sshport/" /etc/ssh/sshd_config
+	sed -i "s/protocol 3,2/protocol 2/" /etc/ssh/sshd_config
+	sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+	sed -i "s/DebianBanner yes/DebianBanner no/" /etc/ssh/sshd_config
+	echo "restarting ssh"
+	sleep 2
+	/etc/init.d/ssh restart -y
+	echo "enabling firewall"
+	sleep 2
+	ufw enable -y
 
 
-echo "############################################"
-echo "# adding $username to sudo and fuse groups #"
-echo "############################################"
-sleep 3
-usermod -a -G sudo $username
-usermod -a -G fuse $username
+	echo "##########################"
+	echo "## secure shared memory ##"
+	echo "##########################"
+	sleep 2
+	echo "tmpfs     /run/shm     tmpfs     defaults,noexec,nosuid     0     0" >> /etc/fstab
+	echo "adding admin group"
+	sleep 2
+	groupadd admin
+	usermod -a -G admin $username
+	echo "protect su by limiting access to admin group only"
+	dpkg-statoverride --update --add $username admin 4750 /bin/su
 
 
-echo "############################"
-echo "## ip spoofing protection ##"
-echo "############################"
-cat > /etc/host.conf << EOF
-order bind,hosts
-nospoof on
+	echo "############################################"
+	echo "# adding $username to sudo and fuse groups #"
+	echo "############################################"
+	sleep 3
+	usermod -a -G sudo $username
+	usermod -a -G fuse $username
+
+
+	echo "############################"
+	echo "## ip spoofing protection ##"
+	echo "############################"
+	cat > /etc/host.conf << EOF
+	order bind,hosts
+	nospoof on
 EOF
 
-echo "##############################"
-echo "# Harden Network with sysctl #"
-echo "##############################"
-sleep 3
+	echo "##############################"
+	echo "# Harden Network with sysctl #"
+	echo "##############################"
+	sleep 3
 
-cat > /etc/sysctl.conf << EOF
+	cat > /etc/sysctl.conf << EOF
 # IP Spoofing protection
-net.ipv4.conf.all.rp_filter = 1
-net.ipv4.conf.default.rp_filter = 1
+	net.ipv4.conf.all.rp_filter = 1
+	net.ipv4.conf.default.rp_filter = 1
 
 # Ignore ICMP broadcast requests
-net.ipv4.icmp_echo_ignore_broadcasts = 1
+	net.ipv4.icmp_echo_ignore_broadcasts = 1
 
 # Disable source packet routing
-net.ipv4.conf.all.accept_source_route = 0
-net.ipv6.conf.all.accept_source_route = 0 
-net.ipv4.conf.default.accept_source_route = 0
-net.ipv6.conf.default.accept_source_route = 0
+	net.ipv4.conf.all.accept_source_route = 0
+	net.ipv6.conf.all.accept_source_route = 0 
+	net.ipv4.conf.default.accept_source_route = 0
+	net.ipv6.conf.default.accept_source_route = 0
 
 # Ignore send redirects
-net.ipv4.conf.all.send_redirects = 0
-net.ipv4.conf.default.send_redirects = 0
+	net.ipv4.conf.all.send_redirects = 0
+	net.ipv4.conf.default.send_redirects = 0
 
 # Block SYN attacks
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_max_syn_backlog = 2048
-net.ipv4.tcp_synack_retries = 2
-net.ipv4.tcp_syn_retries = 5
+	net.ipv4.tcp_syncookies = 1
+	net.ipv4.tcp_max_syn_backlog = 2048
+	net.ipv4.tcp_synack_retries = 2
+	net.ipv4.tcp_syn_retries = 5
 
 # Log Martians
-net.ipv4.conf.all.log_martians = 1
-net.ipv4.icmp_ignore_bogus_error_responses = 1
+	net.ipv4.conf.all.log_martians = 1
+	net.ipv4.icmp_ignore_bogus_error_responses = 1
 
 # Ignore ICMP redirects
-net.ipv4.conf.all.accept_redirects = 0
-net.ipv6.conf.all.accept_redirects = 0
-net.ipv4.conf.default.accept_redirects = 0 
-net.ipv6.conf.default.accept_redirects = 0
+	net.ipv4.conf.all.accept_redirects = 0
+	net.ipv6.conf.all.accept_redirects = 0
+	net.ipv4.conf.default.accept_redirects = 0 
+	net.ipv6.conf.default.accept_redirects = 0
 
 # Ignore Directed pings
-net.ipv4.icmp_echo_ignore_all = 1
+	net.ipv4.icmp_echo_ignore_all = 1
 EOF
 
-sysctl -p
+	sysctl -p
 
 
-echo "#######################"
-echo "# installing fail2ban #"
-echo "#######################"
-sleep 2
-sudo apt-get install fail2ban -y
-echo "setting up fail2ban"
-sed -i 's/enabled = false/enabled = true/' /etc/fail2ban/jail.conf
-sed -i 's/port = sshd/port = $SSHPORT/' /etc/fail2ban/jail.conf
-sed -i 's/port = sshd/port = $SSHPORT/' /etc/fail2ban/jail.conf
-sed -i 's/maxretry = 5/maxretry = 3/' /etc/fail2ban/jail.conf
+	echo "#######################"
+	echo "# installing fail2ban #"
+	echo "#######################"
+	sleep 2
+	sudo apt-get install fail2ban -y
+	echo "setting up fail2ban"
+	sed -i 's/enabled = false/enabled = true/' /etc/fail2ban/jail.conf
+	sed -i 's/port = sshd/port = $SSHPORT/' /etc/fail2ban/jail.conf
+	sed -i 's/port = sshd/port = $SSHPORT/' /etc/fail2ban/jail.conf
+	sed -i 's/maxretry = 5/maxretry = 3/' /etc/fail2ban/jail.conf
 
 
-echo "###################################"
-echo "## installing squid proxy server ##"
-echo "###################################"
-sleep 2
-sudo apt-get install squid3 squid3-common -y
+	echo "###################################"
+	echo "## installing squid proxy server ##"
+	echo "###################################"
+	sleep 2
+	sudo apt-get install squid3 squid3-common -y
 
-cat > /etc/squid3/squid.conf << EOF
-http_port $SQUIDPORT
-via off
-forwarded_for off
-request_header_access Allow allow all
-request_header_access Authorization allow all 
-request_header_access WWW-Authenticate allow all 
-request_header_access Proxy-Authorization allow all 
-request_header_access Proxy-Authenticate allow all 
-request_header_access Cache-Control allow all 
-request_header_access Content-Encoding allow all 
-request_header_access Content-Length allow all 
-request_header_access Content-Type allow all 
-request_header_access Date allow all 
-request_header_access Expires allow all 
-request_header_access Host allow all 
-request_header_access If-Modified-Since allow all
-request_header_access Last-Modified allow all 
-request_header_access Location allow all 
-request_header_access Pragma allow all 
-request_header_access Accept allow all 
-request_header_access Accept-Charset allow all 
-request_header_access Accept-Encoding allow all 
-request_header_access Accept-Language allow all 
-request_header_access Content-Language allow all 
-request_header_access Mime-Version allow all 
-request_header_access Retry-After allow all 
-request_header_access Title allow all 
-request_header_access Connetcion allow all 
-request_header_access Proxy-Connetcion allow all 
-request_header_access User-Agent allow all 
-request_header_access Cookie allow all 
-request_header_access All deny all
-http_access allow ncsa_auth
+	cat > /etc/squid3/squid.conf << EOF
+	http_port $SQUIDPORT
+	via off
+	forwarded_for off
+	request_header_access Allow allow all
+	request_header_access Authorization allow all 
+	request_header_access WWW-Authenticate allow all 
+	request_header_access Proxy-Authorization allow all 
+	request_header_access Proxy-Authenticate allow all 
+	request_header_access Cache-Control allow all 
+	request_header_access Content-Encoding allow all 
+	request_header_access Content-Length allow all 
+	request_header_access Content-Type allow all 
+	request_header_access Date allow all 
+	request_header_access Expires allow all 
+	request_header_access Host allow all 
+	request_header_access If-Modified-Since allow all
+	request_header_access Last-Modified allow all 
+	request_header_access Location allow all 
+	request_header_access Pragma allow all 
+	request_header_access Accept allow all 
+	request_header_access Accept-Charset allow all 
+	request_header_access Accept-Encoding allow all 
+	request_header_access Accept-Language allow all 
+	request_header_access Content-Language allow all 
+	request_header_access Mime-Version allow all 
+	request_header_access Retry-After allow all 
+	request_header_access Title allow all 
+	request_header_access Connetcion allow all 
+	request_header_access Proxy-Connetcion allow all 
+	request_header_access User-Agent allow all 
+	request_header_access Cookie allow all 
+	request_header_access All deny all
+	http_access allow ncsa_auth
 EOF
 
-apt-get install apache2-utils -y
-echo "" >> /etc/squid3/squid_passwd
-touch /etc/squid3/squid_passwd
-chmod 777 /etc/squid3/squid_passwd
-htpasswd -b -c /etc/squid3/squid_user $SQUIDUSER $SQUIDPASS
-service squid3 stop
-service squid3 start
-echo "squid started on port $SQUIDPORT #"
+	apt-get install apache2-utils -y
+	echo "" >> /etc/squid3/squid_passwd
+	touch /etc/squid3/squid_passwd
+	chmod 777 /etc/squid3/squid_passwd
+	htpasswd -b -c /etc/squid3/squid_user $SQUIDUSER $SQUIDPASS
+	service squid3 stop
+	service squid3 start
+	echo "squid started on port $SQUIDPORT #"
 
 
 
